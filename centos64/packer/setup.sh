7a801c906921e5325e3b83e030fe139fcb20ff68
yoshida <yoshida@vasily.jp>
2014-03-17 19:00:44 +0900

    update

--- centos64/packer/setup.sh
+++ centos64/packer/setup.sh
@@ -1,5 +1,16 @@
+# Execute by non-root user
+
 # ssh
 mkdir -pm 700 /home/vagrant/.ssh
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
+
+# rbenv && ruby-build
+export RBENV_ROOT=/usr/local/rbenv
+export PATH=${RBENV_ROOT}/bin:$PATH
+rbenv install 2.0.0-p451
+rbenv install 1.9.2-p326
+
+# system ruby
+PREFIX=/usr/local sudo ruby-build 2.0.0-p451 2.0.0-p451
